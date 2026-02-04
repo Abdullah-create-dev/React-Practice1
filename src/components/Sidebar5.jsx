28c1276651ef01652e4cdc9b0b8dabfedf8d5caf
abdullah <a21744693@gmail.com>
2026-02-04 18:55:53 +0500

    second commit

--- src/components/Sidebar5.jsx
+++ src/components/Sidebar5.jsx
@@ -1,4 +1,4 @@
-const Sidebar5 = () => {
+const SideBar5 = () => {
   return (
     <div className="border-4 flex flex-1 justify-center items-center px-8 h-100 text-4xl bg-green-500">
       Sidebar
@@ -6,4 +6,4 @@ const Sidebar5 = () => {
   )
 }
 
-export default Sidebar5
+export default SideBar5
